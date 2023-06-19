a9b2e030040deba7f21136cbc28297a654443b34
Veysel Erdemci <v.erdemci@gmail.com>
2023-06-19 20:18:04 +0300

    Node DB1 Projesi

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,37 @@
+const db = require("../../data/db-config");
+
 const getAll = () => {
   // KODLAR BURAYA
+  //select * from accounts
+  return db("accounts");
 }
 
 const getById = id => {
   // KODLAR BURAYA
+  //default select için return array formatındadır. First yapınca object {} formatında dönüyor.
+  return db("accounts").where("id",id).first();
 }
 
-const create = account => {
-  // KODLAR BURAYA
+const getByName = name =>{
+  //select * from accounts where name = '{name}' limit 1
+  return db("accounts").where("name",name).first();
 }
 
-const updateById = (id, account) => {
+const create = async (account) => {
   // KODLAR BURAYA
+  const [id] = await db("accounts").insert(account);
+  return getById(id);
+}
+
+const updateById = async (id, account) => {
+  // KODLAR BURAYA
+ await  db("accounts").where("id",id).update(account);
+  return getById(id);
 }
 
 const deleteById = id => {
   // KODLAR BURAYA
+  return db("accounts").where("id",id).del();
 }
 
 module.exports = {
@@ -24,4 +40,5 @@ module.exports = {
   create,
   updateById,
   deleteById,
+  getByName
 }
